5f5c413afe871198fd1cb3245b7e619f0ac40eea
Uday Jamgade <udayjamgade1@gmail.com>
2024-10-29 15:23:34 +0530

    api give error

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const apikey= "a2cfd4fd50a5420a975e8fba1af7a1f0";
+
+ const container = document.getElementsByClassName("container");
 
 async function getdata() {
-    try{
-        const response= await fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=${apikey}`);
+    try {
+
+        const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=a2cfd4fd50a5420a975e8fba1af7a1f0`
+        const response= await fetch(url);
         const data = await response.json();
+        return data.articles;
 
 
-    }catch(error){
+    } catch(error){
         console.log("error found ",error);
         return [];
 
@@ -14,3 +18,37 @@ async function getdata() {
     
 }
 
+function displayBlogs(articles){
+    container.innerHTML=" ";
+
+    articles.forEach((article)=>{
+        const card=  document.createElement("div");
+        card.classList.add=" card";
+        const img = document.createElement("img");
+        img.src= article.urlToImage;
+        img.alt=article.title;
+        const title = document.createElement('h1');
+        title.textContent= article.title;
+        const description = document.createElement('p');
+        description.textContent= article.description;
+
+        
+        card.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(description);
+        container.appendChild(card);
+        
+        
+    });
+
+}
+
+
+(async ()=>{
+    try {
+        const articles = await getdata();
+         displayBlogs(articles);
+     } catch(error){
+        console.log("error found ",error);
+    }
+})();
