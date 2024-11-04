fbdc9093c3a681e0b3ba8ee1f0da71c5169ea242
Uday Jamgade <udayjamgade1@gmail.com>
2024-11-04 23:35:34 +0530

    comleted

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 
- const container = document.getElementsByClassName("container");
+ const container = document.querySelector(".container");
+ const searchinput=document.querySelector(".input");
+ const searchbutton=document.querySelector("#Serch-button");
+
+ 
+
+  searchbutton.addEventListener("click",async ()=>{
+    const quary = searchinput.value.trim();
+    if(quary !== ""){
+        try{
+            const articles = await fetchNewsQuary(quary);
+            displayBlogs(articles);
+
+        }catch(error){
+           console.log("error in Quary" ,error);
+        }
+    }
+ })
+
+ async function fetchNewsQuary(quary){
+    try {
+
+        const url = `https://newsapi.org/v2/everything?q=${quary}&pageSize=10&apikey=a2cfd4fd50a5420a975e8fba1af7a1f0`
+        const response= await fetch(url);
+        const data = await response.json();
+        return data.articles;
+
+
+    } catch(error){
+        console.log("error found ",error);
+        return [];
+
+    }
+ }
+
 
 async function getdata() {
     try {
@@ -23,20 +57,29 @@ function displayBlogs(articles){
 
     articles.forEach((article)=>{
         const card=  document.createElement("div");
-        card.classList.add=" card";
+        card.classList.add("card");
         const img = document.createElement("img");
         img.src= article.urlToImage;
         img.alt=article.title;
-        const title = document.createElement('h1');
-        title.textContent= article.title;
+        const title = document.createElement('h2');
+        const titlength = article.title.length > 30 ? article.title.slice(0,30)+"....." : article.title;
+        title.textContent= titlength;
+        // title.textContent= article.title;
         const description = document.createElement('p');
+        // const deslength = article.description.length > 120 ? article.title.slice(0,120)+"....." : article.description;
+        // description.textContent= deslength; 
+
         description.textContent= article.description;
 
         
         card.appendChild(img);
         card.appendChild(title);
         card.appendChild(description);
+        card.addEventListener("click",function(){
+            window.open(article.url, "_blanc");
+        })
         container.appendChild(card);
+        
         
         
     });
